2ac11f19eaa1c205e3a081113b98a08656a2a4df
japhethnyaranga <nyaranga4@gmail.com>
2024-03-06 13:51:19 +0300

    first commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,8 @@
+@extends('index')
+
+@section('title', 'Login Page')
+
+@section('content')
+    <!-- Your specific content for this view goes here -->
+    <button type="button" class="btn btn-primary">Login Page</button>
+@endsection
